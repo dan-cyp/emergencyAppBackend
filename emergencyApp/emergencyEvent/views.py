6cef2f027064d2e56983ffeaf32866fd6147c6fd
dan-ha <daniel.hamarik@proton.me>
2023-03-10 08:46:37 +0100

    web socket on edit

--- emergencyApp/emergencyEvent/views.py
+++ emergencyApp/emergencyEvent/views.py
@@ -55,6 +55,12 @@ class EmergencyEventViewSet(viewsets.ViewSet):
         serializer = EmergencyEventReceiveSerializer(data=request.data)
         if serializer.is_valid():
 
+
+            # Retrieve the channel layer
+            channel_layer = get_channel_layer()
+            group_name = "emergencyEvents"
+
+
             citizenId = request.data["citizenId"]
 
             #Get all EmergencyEvents
@@ -65,18 +71,27 @@ class EmergencyEventViewSet(viewsets.ViewSet):
             for ee in emergencyEventsAll:
                 if ee["citizen"]["id"] == citizenId:
                     emergencyEventsCitizen.append(ee)
-
-            latestEmergencyEvent = emergencyEventsCitizen[len(emergencyEventsCitizen) - 1]
+            
+            latestEmergencyEvent = None
+            if len(emergencyEventsCitizen) > 0:
+                latestEmergencyEvent = emergencyEventsCitizen[len(emergencyEventsCitizen) - 1]
 
             # CREATE NEW EmergencyEvent - nothing yet there
-            if len(emergencyEventsCitizen) == 0 or latestEmergencyEvent["checked"]:
+            if latestEmergencyEvent == None or len(emergencyEventsCitizen) == 0 or latestEmergencyEvent["checked"]:
                 print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 print("NEW EMERGENCY: " + str(request.data))
                 print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                serializer.save()
-                transaction.commit()
-                # notify channels!!! <-- HERE -->
-                return Response(serializer.data, status=201)
+
+                event = serializer.create(serializer.validated_data)
+                response_serializer = EmergencyEventSerializer(event)
+
+                # notify channels!!! 
+                newEmergencyEvent = response_serializer.data
+                # this needs to be there in order to send
+                newEmergencyEvent["type"] = "send_message"
+                async_to_sync(channel_layer.group_send)(group_name, newEmergencyEvent)
+
+                return Response(response_serializer.data, status=201)
             else:
                 print("SHOULD UPDATEEEEEEEE")
                 # get by id and update
@@ -90,7 +105,11 @@ class EmergencyEventViewSet(viewsets.ViewSet):
                 serializerToUpdate = self.serializerZero(emergencyEventToUpdate, {"poss": poss} , partial=True)
                 serializerToUpdate.is_valid(raise_exception=True)
                 serializerToUpdate.save()
-                # notify channels!!! <-- HERE -->
+                # notify channels!!! 
+                newEmergencyEvent = serializerToUpdate.data
+                # this needs to be there in order to send
+                newEmergencyEvent["type"] = "send_message"
+                async_to_sync(channel_layer.group_send)(group_name, newEmergencyEvent)
                 print("UPDATEEEEED SUCCESFULLY")
                 return Response(serializerToUpdate.data, status=200)
 
